49adf2f082be666060010ca30bd56a0f2eccdc64
didi <lijiancheng0614@gmail.com>
2017-09-04 20:53:15 +0800

    Add codes.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # android-TFDetect
+
+This sample demonstrates how to use [TensorFlow Android Inference Interface](https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/android) and [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/object_detection/) to do object detection in android.
+
+
+## Pre-requisites
+
+- Android Studio
+
+- Android SDK v21
+
+- Android Build Tools v21.1.1
+
+- Android Support Repository
+
+
+## Getting Started
+
+1. Download the model and labels_list.
+
+```bash
+# From android-TFDetect/assets/
+wget https://storage.googleapis.com/download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_android_export.zip
+unzip ssd_mobilenet_v1_android_export.zip
+rm ssd_mobilenet_v1_android_export.zip
+```
+
+2. Build.
+
+This sample uses the Gradle build system. To build this project, use the "gradlew build" command or use "Import Project" in Android Studio.
---
+++ src/org/tensorflow/demo/DetectorActivity.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.demo;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.SystemClock;
+import android.os.Trace;
+import android.util.Size;
+import android.util.TypedValue;
+import android.view.Display;
+import android.widget.Toast;
+
+import org.tensorflow.demo.OverlayView.DrawCallback;
+import org.tensorflow.demo.env.BorderedText;
+import org.tensorflow.demo.env.ImageUtils;
+import org.tensorflow.demo.env.Logger;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
+    private static final Logger LOGGER = new Logger();
+
+    private static final int TF_OD_API_INPUT_SIZE = 300;
+    private static final String TF_OD_API_MODEL_FILE =
+            "file:///android_asset/ssd_mobilenet_v1_android_export.pb";
+    private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco_labels_list.txt";
+    // Minimum detection confidence to track a detection.
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.6f;
+    private static final boolean MAINTAIN_ASPECT = false;
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+    private static final float TEXT_SIZE_DIP = 10;
+    private Integer sensorOrientation;
+
+    private Classifier detector;
+
+    private int previewWidth = 0;
+    private int previewHeight = 0;
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private Bitmap cropCopyBitmap;
+
+    private BorderedText borderedText;
+
+    private long lastProcessingTimeMs;
+
+    @Override
+    public void onPreviewSizeChosen(final Size size, final int rotation) {
+        final float textSizePx =
+                TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+        borderedText = new BorderedText(textSizePx);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        int cropSize = TF_OD_API_INPUT_SIZE;
+
+        try {
+            detector = TensorFlowObjectDetectionAPIModel.create(
+                    getAssets(), TF_OD_API_MODEL_FILE, TF_OD_API_LABELS_FILE, TF_OD_API_INPUT_SIZE);
+            cropSize = TF_OD_API_INPUT_SIZE;
+        } catch (final IOException e) {
+            LOGGER.e("Exception initializing classifier!", e);
+            Toast toast =
+                    Toast.makeText(
+                            getApplicationContext(), "Classifier could not be initialized", Toast.LENGTH_SHORT);
+            toast.show();
+            finish();
+        }
+
+        previewWidth = size.getWidth();
+        previewHeight = size.getHeight();
+
+        final Display display = getWindowManager().getDefaultDisplay();
+        final int screenOrientation = display.getRotation();
+
+        LOGGER.i("Sensor orientation: %d, Screen orientation: %d", rotation, screenOrientation);
+
+        sensorOrientation = rotation + screenOrientation;
+
+        LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Config.ARGB_8888);
+
+        frameToCropTransform =
+                ImageUtils.getTransformationMatrix(
+                        previewWidth, previewHeight,
+                        cropSize, cropSize,
+                        sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        addCallback(
+                new DrawCallback() {
+                    @Override
+                    public void drawCallback(final Canvas canvas) {
+                        if (!isDebug()) {
+                            return;
+                        }
+                        final Bitmap copy = cropCopyBitmap;
+                        if (copy == null) {
+                            return;
+                        }
+
+                        final int backgroundColor = Color.argb(100, 0, 0, 0);
+                        canvas.drawColor(backgroundColor);
+
+                        final Matrix matrix = new Matrix();
+                        final float scaleFactor = 2;
+                        matrix.postScale(scaleFactor, scaleFactor);
+                        matrix.postTranslate(
+                                canvas.getWidth() - copy.getWidth() * scaleFactor,
+                                canvas.getHeight() - copy.getHeight() * scaleFactor);
+                        canvas.drawBitmap(copy, matrix, new Paint());
+
+                        final Vector<String> lines = new Vector<>();
+                        if (detector != null) {
+                            final String statString = detector.getStatString();
+                            final String[] statLines = statString.split("\n");
+                            Collections.addAll(lines, statLines);
+                        }
+                        lines.add("");
+
+                        lines.add("Frame: " + previewWidth + "x" + previewHeight);
+                        lines.add("Crop: " + copy.getWidth() + "x" + copy.getHeight());
+                        lines.add("View: " + canvas.getWidth() + "x" + canvas.getHeight());
+                        lines.add("Rotation: " + sensorOrientation);
+                        lines.add("Inference time: " + lastProcessingTimeMs + "ms");
+
+                        borderedText.drawLines(canvas, 10, canvas.getHeight() - 10, lines);
+                    }
+                });
+    }
+
+    protected void processImageRGBbytes(int[] rgbBytes) {
+        rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
+        final Canvas canvas = new Canvas(croppedBitmap);
+        canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+
+        // For examining the actual TF input.
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(croppedBitmap);
+        }
+
+        runInBackground(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        final long startTime = SystemClock.uptimeMillis();
+                        final List<Classifier.Recognition> results = detector.recognizeImage(croppedBitmap);
+                        lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+                        LOGGER.i("Detect: %s", results);
+                        cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+                        final Canvas canvas = new Canvas(cropCopyBitmap);
+                        final Paint paint = new Paint();
+                        paint.setColor(Color.RED);
+                        paint.setStyle(Style.STROKE);
+                        paint.setStrokeWidth(2.0f);
+
+                        final List<Classifier.Recognition> mappedRecognitions =
+                                new LinkedList<>();
+
+                        for (final Classifier.Recognition result : results) {
+                            final RectF location = result.getLocation();
+                            if (location != null && result.getConfidence() >= MINIMUM_CONFIDENCE_TF_OD_API) {
+                                canvas.drawRect(location, paint);
+
+                                cropToFrameTransform.mapRect(location);
+                                result.setLocation(location);
+                                mappedRecognitions.add(result);
+                            }
+                        }
+
+                        requestRender();
+                        computing = false;
+                    }
+                });
+
+        Trace.endSection();
+    }
+
+    @Override
+    public void onImageAvailable(final ImageReader reader) {
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.camera_connection_fragment_tracking;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+    @Override
+    public void onSetDebug(final boolean debug) {
+        detector.enableStatLogging(debug);
+    }
+}
